7e02d93517e93437e1681ead64a935dfffdcae6e
giannagalard <giannagalard@gmail.com>
2021-12-21 20:50:40 -0500

    FEAT: Finished most of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import "../styles/Home.css";
+
 export default function Home() {
-    
-    return (
-        <div>
-            
+  return (
+    <div className="bg-image">
+      <div className="welcome">
+        <div className="textContainer">
+          <h1 className="welcomeText">
+            Welcome To <br /> Lofi Git
+          </h1>{" "}
         </div>
-    )
+        <div className="buttonContainer">
+          {" "}
+          <button className="loginButton">Login</button>{" "}
+            <h3 className="orText">OR</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
