d92a5f8801515132f23b0f919ea98e92ef5fe287
AndreaHabib <andrea.atef@yahoo.com>
2021-12-22 17:55:08 -0500

    FEAT: polish notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,22 +1,22 @@
 import "../styles/NotFound.css";
 import { Link } from "react-router-dom";
+import error from "../images/404.gif";
 
 export default function NotFound() {
   return (
     <div className="uwuOwO">
-      <div className="nf-image">
-        <div className="oopsieWoopsie">
-          <div className="notFoundTextContainer">
-            <h1 className="nfText">404</h1>
-            <h3 className="nfSubHeader">page not found</h3>
-            <h3 className="nfMicroSubHeader">:-(</h3>
+      <div className="oopsieWoopsie">
+        <div className="notFoundTextContainer">
+          <h1 className="nfText">404</h1>
+          <h3 className="nfSubHeader">page not found</h3>
+          <h3 className="nfMicroSubHeader">:-(</h3>
+          <img className="error" src={error} alt="404 error" />
+          <div className="nfButtonContainer">
+            <Link className="backButton" to="/home">
+              Back
+            </Link>
           </div>
         </div>
-      </div>
-      <div className="nfButtonContainer">
-        <Link className="backButton" to="/home">
-          Back
-        </Link>
       </div>
     </div>
   );
