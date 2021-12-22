577369188962852b8b6f8afd620b6926e9a76a74
giannagalard <giannagalard@gmail.com>
2021-12-21 23:03:57 -0500

    FEAT: Functional search button

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            <Route path="user/:username" element={<User />} />
+            <Route path="/user/:username" element={<User />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,12 +1,14 @@
 import "../styles/SearchBar.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-
-export default function searchBar() {
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
   return (
     <div className = "searchContainer">
         <i className = "fa fa-search searchIcon"></i>
-        <input className = "searchBar" type = "search" placeholder = "Search for a user"/>
-        <input type = "submit" value = "Search" className = "searchButton"/>
+        <input onChange = {(e) => setSearch(e.target.value)}className = "searchBar" type = "search" placeholder = "Search for a user"/>
+        <Link to = {{pathname:`/user/${search}`}} className = "searchButton "> Submit </Link>
     </div>
   );
 }
