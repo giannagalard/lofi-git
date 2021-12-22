19f15749fe12ea6498ed19e5b9dad2cba803337e
giannagalard <giannagalard@gmail.com>
2021-12-22 17:24:35 -0500

    FIX: mobile view broke desktop view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,49 @@
 import SearchBar from "../components/SearchBar";
 import "../styles/Home.css";
+import { Box } from "@mui/system";
 
 export default function Home() {
   return (
-    <div className="bg-image">
-      <div className="welcome">
-        <div className="textContainer">
-          <h1 className="welcomeText">
-            Welcome To <br /> Lofi Git
-          </h1>{" "}
+    <>
+      <Box
+        component="div"
+        sx={{ display: { xs: "none", md: "block" } }}
+        className="welcome"
+      >
+        <div className="bg-image">
+          <div className="textContainer">
+            <h1 className="welcomeText">
+              Welcome To <br /> Lofi Git
+            </h1>{" "}
+          </div>
+          <div className="buttonContainer">
+            {" "}
+            <button className="loginButton">Login</button>{" "}
+            <h3 className="orText">OR</h3>
+            <SearchBar />
+          </div>
         </div>
-        <div className="buttonContainer">
-          {" "}
-          <button className="loginButton">Login</button>{" "}
-          <h3 className="orText">OR</h3>
-          <SearchBar />
+      </Box>
+
+      <Box
+        component="div"
+        sx={{ display: { xs: "block", lg: "none" } }}
+        className="welcome"
+      >
+        <div className="phone-image">
+          <div className="mobileTextContainer">
+            <h1 className="welcomeText">
+              Welcome To <br /> Lofi Git
+            </h1>{" "}
+          </div>
+          <div className="mobileButtonContainer">
+            {" "}
+            <button className="loginButton">Login</button>{" "}
+            <h3 className="orText">OR</h3>
+            <SearchBar />
+          </div>
         </div>
-      </div>
-    </div>
+      </Box>
+    </>
   );
 }
