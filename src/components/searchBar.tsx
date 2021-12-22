633ac6a3f62cd134d2b0bdaa05290df167dfa3d6
giannagalard <giannagalard@gmail.com>
2021-12-21 23:27:52 -0500

    FEAT: On key press submit

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,14 +1,28 @@
 import "../styles/SearchBar.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
+  let navigate = useNavigate();
   const [search, setSearch] = useState("");
+  const clickyBlicky = (e: any) => {
+    if (e.key === "Enter"){navigate (`/user/${search}`)}
+  };
   return (
-    <div className = "searchContainer">
-        <i className = "fa fa-search searchIcon"></i>
-        <input onChange = {(e) => setSearch(e.target.value)}className = "searchBar" type = "search" placeholder = "Search for a user"/>
-        <Link to = {{pathname:`/user/${search}`}} className = "searchButton "> Submit </Link>
+    <div className="searchContainer">
+      <i className="fa fa-search searchIcon"></i>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        className="searchBar"
+        type="search"
+        placeholder="Search for a user"
+        onKeyPress = {(e) => clickyBlicky(e)}
+      />
+      <Link to={{ pathname: `/user/${search}` }} className="searchButton ">
+        {" "}
+        Submit{" "}
+      </Link>
     </div>
   );
 }
