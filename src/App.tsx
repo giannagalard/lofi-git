d817a94ed34f38995fa5b2da89ecf901010f2abb
AndreaHabib <andrea.atef@yahoo.com>
2021-12-23 18:32:54 -0500

    FEAT: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import Home from "./pages/Home";
 import User from "./pages/User";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Footer } from "./components/Footer";
 // import CherryBomb from "./fonts/CherryBomb.ttf";
 
 function App() {
+  let location = useLocation();
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/user/:username" element={<User />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/user/:username" element={<User />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer location={location.pathname} />
       </header>
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
-export default function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+import "../styles/Footer.css";
+
+interface prop {
+  location: string;
 }
+
+export const Footer: React.FC<prop> = (props) => {
+  return (
+    <footer id={props.location === "/home" ? "footer" : "footerUser"}>
+      <p className="footerText">
+        &copy; COPYRIGHT 2021{" "}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/andrea-habib-730941198/"
+        >
+          Andrea Habib
+        </a>{" "}
+        &{" "}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/giannagalard/"
+        >
+          Gianna Galard
+        </a>
+      </p>
+    </footer>
+  );
+};
