5f4ff74452eb977dc90b5cec85cc2213382e392d
AndreaHabib <andrea.atef@yahoo.com>
2021-12-23 21:00:41 -0500

    FEAT: added music player

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,47 @@
 import SearchBar from "../components/SearchBar";
 import "../styles/Home.css";
 import { Box } from "@mui/system";
+import lofi from "../sound/MoonlightDenetsuLofi.wav";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 export default function Home() {
   return (
     <>
       <Box
         component="div"
-        sx={{ display: { xs: "none", sm: "none", md: "none", lg: "block" } }}
+        sx={{ display: { xs: "none", sm: "none", md: "block", lg: "block" } }}
         className="welcome"
       >
         <div className="bg-image">
-          <div className="textContainer">
-            <h1 className="welcomeText">
-              Welcome To <br /> Lofi Git
-            </h1>{" "}
-          </div>
-          <div className="buttonContainer">
-            {/* {" "} 
+          <div className="upperContainer">
+            <div className="textContainer">
+              <h1 className="welcomeText">
+                Welcome To <br /> Lofi Git
+              </h1>{" "}
+            </div>
+            <div className="buttonContainer">
+              {/* {" "} 
             <button className="loginButton">Login</button>{" "}
             <h3 className="orText">OR</h3> */}
-            <div style={{ width: "30%" }}>
-              <SearchBar />
+              <div style={{ width: "35%" }}>
+                <SearchBar />
+              </div>
             </div>
+          </div>
+          <div className="audioContainer">
+            <AudioPlayer
+              style={{ marginLeft: "100px", width: "60%" }}
+              autoPlay
+              src={lofi}
+            />
           </div>
         </div>
       </Box>
 
       <Box
         component="div"
-        sx={{ display: { xs: "block", sm: "block", md: "block", lg: "none" } }}
+        sx={{ display: { xs: "block", sm: "block", md: "none", lg: "none" } }}
         className="welcome"
       >
         <div className="phone-image">
@@ -43,6 +55,19 @@ export default function Home() {
             <button className="loginButton">Login</button>{" "}
             <h3 className="orText">OR</h3> */}
             <SearchBar />
+          </div>
+          <div
+            style={{
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <AudioPlayer
+              style={{ width: "95%", marginTop: "30px" }}
+              autoPlay
+              src={lofi}
+            />
           </div>
         </div>
       </Box>
